0a36901af015caf6dfa20980b73e7bb3802df78e
SharonLagat <32391063+SharonLagat@users.noreply.github.com>
2018-06-19 13:18:40 +0200

    Update header.php

--- manage-site/header.php
+++ manage-site/header.php
@@ -1,9 +1,7 @@
 <?php
 /*
  * @author Sharon Lagat
- * @facebook https://www.facebook.com/slagat7
- * @twitter https://twitter.com/LagatSharon
- * @instagram https://www.instagram.com/sharonlagat/
+ * @googleplus https://plus.google.com/sharonlagat/
  */
 ?>
 <!DOCTYPE html>
